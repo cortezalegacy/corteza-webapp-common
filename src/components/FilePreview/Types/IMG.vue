dac9ca052eab3efed44f556de9e81d41d3854f40
Mia Arh <mia@kendu.si>
2019-06-21 18:03:09 +0200

    Increase img preview width

--- src/components/FilePreview/Types/IMG.vue
+++ src/components/FilePreview/Types/IMG.vue
@@ -1,8 +1,6 @@
 <template>
   <div :class="{ inline }">
-    <div :class="{ inline }">
-      <img ref="image" @click="$emit('openPreview', {})" :key="src" :src="src" :title="title" :alt="alt" :class="getClass" :style="previewStyle" @error.once="reloadBrokenImage" @load="loaded=true">
-    </div>
+    <img ref="image" @click="$emit('openPreview', {})" :key="src" :src="src" :title="title" :alt="alt" :class="getClass" :style="previewStyle" @error.once="reloadBrokenImage" @load="loaded=true">
   </div>
 </template>
 
@@ -59,34 +57,29 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 div {
   margin: 0 auto;
   object-fit: contain;
-}
-div:not(.inline) {
-  margin: 20px auto;
-  max-width: 500px;
-}
-div:not(.inline) > div {
-  margin: 0 20px;
-}
-img {
-  box-shadow: 0 0 3px #1E1E1E41;
-  box-sizing: border-box;
-}
-div.inline img {
-  width: 100%;
-}
-div:not(.inline) img {
-  max-width: 100%;
-}
-
-div.inline img {
-  cursor: zoom-in;
-}
-img.loaded {
-  width: 100%;
-  height: 100%;
+  img {
+    &.loaded {
+      width: 75%;
+      height: 100%;
+      margin: 0 auto;
+      display: block;
+    }
+  }
+  &:not(.inline) {
+    margin: 20px auto;
+    img {
+      max-width: 100%;
+    }
+  }
+  &.inline {
+    img {
+      width: 100%;
+      cursor: zoom-in;
+    }
+  }
 }
 </style>
