49d36436b65f30fd8f5d27000c8604cf0ed5b743
Denis Arh <denis.arh@gmail.com>
2019-08-28 12:13:34 +0200

    Better metadata handling for namespaces

--- src/lib/types/compose/namespace.js
+++ src/lib/types/compose/namespace.js
@@ -5,10 +5,20 @@ export default class Namespace extends ComposeObject {
   constructor (n = {}) {
     super()
     this.namespaceID = PropCast(ID, n.namespaceID)
-    this.name = PropCast(String, n.name)
-    this.slug = PropCast(String, n.slug)
+    this.name = PropCast(String, n.name, '')
+    this.slug = PropCast(String, n.slug, '')
     this.enabled = !!n.enabled
-    this.meta = PropCast(Object, n.meta)
+
+    this.meta = {
+      'subtitle': '',
+      'description': '',
+    }
+
+    if (n.meta) {
+      Object.keys(this.meta).forEach(k => {
+        this.meta[k] = n.meta[k] || undefined
+      })
+    }
 
     this.createdAt = PropCast(ISO8601, n.createdAt)
     this.updatedAt = PropCast(ISO8601, n.updatedAt)
