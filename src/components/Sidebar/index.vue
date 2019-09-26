bb43e11fb901fd27ac40dc956954fec07b1f5074
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-09-26 11:31:32 +0200

    Prep a general sidebar component

---
+++ src/components/Sidebar/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <aside class="sidebar d-flex flex-column vh-100"
+         :class="[ position, visible ? 'visible' : null ]"
+         :style="getStyle">
+
+    <portal :to="`name-${name}`">
+      <b-button variant="link"
+                class="p-0 text-dark"
+                @click="toggle">
+
+        <font-awesome-icon :icon="['fas', 'bars']" />
+      </b-button>
+    </portal>
+
+    <header v-if="$slots.header"
+            class="p-0 border-0 border-bottom-primary">
+
+      <slot name="header" />
+    </header>
+
+    <div class="bg-white overflow-auto flex-grow-1 overflow-auto">
+      <slot />
+    </div>
+
+    <footer v-if="$slots.footer"
+                      class="p-0 border-0 border-bottom-primary">
+
+      <slot name="footer" />
+    </footer>
+  </aside>
+</template>
+
+<script>
+import base from './base'
+
+export default {
+  extends: base,
+  props: {
+    position: {
+      type: String,
+      required: false,
+      default: 'left',
+    },
+
+    width: {
+      type: Number,
+      required: false,
+      default: 200,
+    },
+
+    startVisible: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    disableToggle: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data () {
+    return {
+      visible: this.startVisible,
+    }
+  },
+  computed: {
+    getStyle () {
+      return {
+        width: `${this.width}px`,
+        [`margin-${this.position}`]: this.visible ? '0px' : `-${this.width}px`,
+      }
+    },
+  },
+
+  methods: {
+    toggle () {
+      if (!this.disableToggle) {
+        this.visible = !this.visible
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  transition: margin .3s;
+}
+</style>
