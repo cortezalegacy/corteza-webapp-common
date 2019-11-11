9568535adec585a548935610f0f6e02fba0f94e3
Jože Fortun <joze.fortun@gmail.com>
2019-11-11 15:54:05 +0100

    Rework Settings plugin

--- src/plugins/settings.js
+++ src/plugins/settings.js
@@ -1,13 +1,22 @@
-export default {
-  install (Vue, { api }) {
-    Vue.prototype.$Settings = {
-      get({ key }) {
-        return api.settingsGet({ key })
-      },
-
-      list({ prefix }) {
-        return api.settingsList({ prefix })
-      }
+export class Settings {
+  constructor ({ api } = {}) {
+    if (!api) {
+      throw new Error('settings.noApi')
     }
+
+    this.current = {}
+    this.api = api
+  }
+
+  async init () {
+    this.api.settingsCurrent().then(settings => {
+      this.current = settings
+    })
+  }
+}
+
+export default {
+  install (Vue, opts) {
+    Vue.prototype.$Settings = new Settings(opts)
   },
 }
