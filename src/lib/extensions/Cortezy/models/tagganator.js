75b6c7f036a1b2a236c56692443133f180c50a54
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-11-19 19:07:25 +0100

    Improve tagganator model
    Make it not static, add more instance methods.

--- src/lib/extensions/Cortezy/models/tagganator.js
+++ src/lib/extensions/Cortezy/models/tagganator.js
@@ -2,10 +2,11 @@
  * TagMatch class represents a match
  */
 class TagMatch {
-  constructor ({ from, to, text, associations }) {
+  constructor ({ from, to, text, tag, associations }) {
     this.from = from
     this.to = to
     this.text = text
+    this.tag = tag
     this.associations = associations
   }
 }
@@ -14,31 +15,72 @@ class TagMatch {
  * Tagganator will attempt to guess the tags that occur inside the input.
  */
 export default class Taganator {
-  static init (tags = []) {
-    Taganator.tags = tags
+  constructor ({ tags = [] }) {
+    this.tags = tags
+  }
+
+  /**
+   * Helper to find a tag from the given model
+   * @todo improve
+   * @param {String} tag Tag to find
+   * @returns {Tag|undefined}
+   */
+  findTag (tag) {
+    return this.tags.find(t => t.tag === tag)
+  }
+
+  /**
+   * Helper to update a tag in a given model
+   * @param {String} tag Tag to update
+   * @param {Tag} nt New tag
+   */
+  updateTag (tag, nt) {
+    const i = this.tags.findIndex(t => t.tag === tag)
+    console.log({ i })
+    if (i <= -1) {
+      return
+    }
+
+    this.tags.splice(i, 1, nt)
+  }
+
+  /**
+   * Helper to add a tag to a given model
+   * @todo improve
+   * @param {String} tag Tag to update
+   * @param {Tag} nt New tag
+   */
+  addTag (tag) {
+    this.tags.push(tag)
   }
 
   // @todo improve this!!
-  static findAll (text = '') {
+  /**
+   * Helper to find all tags from this model from a given string
+   * @todo improve this
+   * @param {String} text Text that should be processed
+   * @returns {Array<TagMatch>}
+   */
+  findAll (text = '') {
     text = text.toLowerCase()
     const matches = []
 
-    for (const tag of Taganator.tags) {
+    for (const tag of this.tags) {
       let i = 0
       let index = 0
       while (i > -1) {
-        i = tag.matcher(text)
-        console.log({ tag, i })
-        if (i === -1) {
+        const match = tag.matcher(text)
+        if (!match) {
           break
         }
 
-        const from = index + i
-        const to = index + i + tag.label.length
+        const { from, to } = match
+        i = match.i
         matches.push(new TagMatch({
           from,
           to,
           associations: tag.associations.map(({ kind }) => kind),
+          tag: tag.tag,
           text: text.slice(from, to),
         }))
 
